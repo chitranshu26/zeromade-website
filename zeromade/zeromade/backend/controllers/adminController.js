46352c2b1145424b59b858aa37f4df3a261a01ce
Chitranshu <manju002100@gmail.com>
2026-02-04 22:38:47 +0530

    Production update

--- zeromade/zeromade/backend/controllers/adminController.js
+++
@@ -1,8 +0,0 @@
-/**
- * Admin-specific logic can be added here.
- * Product CRUD and materials are in productController / materialController.
- * User list is in userController. This file is for future admin-only actions.
- */
-
-// Placeholder - admin features are in product/material/user controllers with adminMiddleware
-module.exports = {};
